8ae2eb222a4d1ac850729f9e58d8c9522c18e084
joedip16 <joedippolito16@gmail.com>
2020-11-11 17:45:55 -0500

    created conditional to check if the time is in the past present or future

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,26 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
 var todaysDate = moment().format('ll');
-$("currentDay").html(time)
+$("#currentDay").html(todaysDate)
+
+var currentHour = moment().hour()
+console.log(currentHour)
+
+var hourRow = 17
+if (currentHour > hourRow){
+    console.log("In the past")
+    $(".9am").addClass("past")
+}
+else if(currentHour < hourRow){
+    console.log("In the future")
+    $(".9am").addClass("future")
+}
+else{
+    console.log("Right now!")
+    $(".9am").addClass("present")
+}
+
+
+});
