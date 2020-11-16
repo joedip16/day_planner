bf0e119e11513edcf568a470fc728d3445903f16
joedip16 <joedippolito16@gmail.com>
2020-11-16 18:23:51 -0500

    working on local storage

--- assets/script.js
+++ assets/script.js
@@ -8,19 +8,60 @@ $("#currentDay").html(todaysDate)
 var currentHour = moment().hour()
 console.log(currentHour)
 
-var hourRow = 17
-if (currentHour > hourRow){
-    console.log("In the past")
-    $(".9am").addClass("past")
-}
-else if(currentHour < hourRow){
-    console.log("In the future")
-    $(".9am").addClass("future")
-}
-else{
-    console.log("Right now!")
-    $(".9am").addClass("present")
-}
+$(".hours").each(function(index){
+    console.log(index)
+    var hourRow = $(this).attr("id")
+    console.log(hourRow)
+    if (currentHour > hourRow){
+        console.log("In the past")
+        $(this).addClass("past")
+        
+    }
+    else if(currentHour < hourRow){
+        console.log("In the future")
+        $(this).addClass("future")
+    }
+    else{
+        console.log("Right now!")
+        $(this).addClass("present")
+    }
+})
+var dataInput = localStorage.getItem("#9")
 
+    $(".saveBtn").on("click", function () {
+    $("#9").val(localStorage.getItem("#9"))
+    localStorage.setItem("9am", dataInput)
+    })
+
+
+
+
+
+// var possibleTimes = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+
+
+// function colorPicker(){
+// for(var i = 0; i < possibleTimes.length; i++){
+//     var hourRow = 21
+//     console.log(hourRow)
+    // if (currentHour > hourRow){
+    //     console.log("In the past")
+    //     $(".10am").addClass("past")
+        
+    // }
+    // else if(currentHour < hourRow){
+    //     console.log("In the future")
+    //     $(".10am").addClass("future")
+    // }
+    // else{
+    //     console.log("Right now!")
+    //     $(".10am").addClass("present")
+    // }
+// }
+
+
+
+// }
 
 });
