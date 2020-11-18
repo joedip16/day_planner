8fab095d62c0391af9ab2a449e09570eeb01b795
joedip16 <joedippolito16@gmail.com>
2020-11-18 18:12:35 -0500

    local storage saves value and prints in text area

--- assets/script.js
+++ assets/script.js
@@ -26,25 +26,17 @@ $(".hours").each(function(index){
         $(this).addClass("present")
     }
 })
-    var retrievedInput = localStorage.getItem("woop")
+    var retrievedInput = localStorage.getItem("key")
     console.log(retrievedInput)
     $("#9").html(retrievedInput)
-    $(".btn").on("click", function () {
-    var storedInput = $(this).prev(".textarea").val(retrievedInput)
-    var divKey = $(this).attr("id")
-    console.log(divKey)
+    $(".saveBtn9").on("click", function () {
+    var storedInput = $("#9").val()
     console.log(storedInput)
-    localStorage.setItem(divKey, storedInput)
+    localStorage.setItem("key", storedInput)
     })
 
 
 
-
-
-
-
-        //find a way to traverse the dom using jquery to grab textarea value next to button that is clicked
-        //think .siblings .prev maybe there is another solution? resturcture html maybe a bettter method?
 
 
 // var possibleTimes = [9, 10, 11, 12, 13, 14, 15, 16, 17]
